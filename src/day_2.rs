1f9156e773548fc6e0becdff9f82666c808fb364
Geoff Shannon <geoffpshannon@gmail.com>
2017-12-02 13:15:58 -0600

    Implement basic row difference algorithm

--- src/day_2.rs
+++ src/day_2.rs
@@ -1,3 +1,10 @@
+fn row_difference(row: Vec<u8>) -> u8 {
+    assert!(row.len() > 0);
+    let max = row.iter().max().unwrap();
+    let min = row.iter().min().unwrap();
+    max - min
+}
+
 pub fn solve(puzzle: &str) -> u32 {
     0
 }
@@ -8,5 +15,15 @@ pub fn solve2(puzzle: &str) -> u32 {
 
 #[cfg(test)]
 mod test {
+    use super::*;
 
+    #[test]
+    fn test_row_difference() {
+        assert_eq!(8, row_difference(vec![5, 1, 9, 5]));
+    }
+
+    #[test]
+    fn test_row_difference2() {
+        assert_eq!(4, row_difference(vec![7, 5, 3]));
+    }
 }
