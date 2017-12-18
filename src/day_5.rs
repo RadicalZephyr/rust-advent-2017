1c46ba73a69fa66f3e437019190808bb8228d49c
Geoff Shannon <geoffpshannon@gmail.com>
2017-12-17 16:46:17 -0800

    Implement day 5 part 1

--- src/day_5.rs
+++ src/day_5.rs
@@ -1,5 +1,44 @@
-pub fn solve(_puzzle: &str) -> u32 {
-    0
+#[derive(Debug)]
+struct Simulator {
+    idx: isize,
+    ticks: u32,
+    program: Vec<i16>,
+}
+
+impl Simulator {
+    pub fn new(program: Vec<i16>) -> Simulator {
+        Simulator {
+            idx: 0,
+            ticks: 0,
+            program
+        }
+    }
+
+    pub fn tick(&mut self) {
+        self.ticks += 1;
+        let increment = self.program[self.idx as usize];
+        self.program[self.idx as usize] += 1;
+        self.idx += increment as isize;
+    }
+
+    pub fn has_escaped(&self) -> bool {
+        self.idx < 0 || self.idx as usize >= self.program.len()
+    }
+
+    pub fn tick_count(&self) -> u32 {
+        self.ticks
+    }
+}
+
+pub fn solve(puzzle: &str) -> u32 {
+    let program: Vec<i16> = puzzle.lines().map(|line| line.parse::<i16>().expect("Should have been a number")).collect();
+    let mut simulator = Simulator::new(program);
+
+    while !simulator.has_escaped() {
+        simulator.tick();
+    }
+
+    simulator.tick_count()
 }
 
 pub fn solve2(_puzzle: &str) -> u32 {
@@ -12,7 +51,7 @@ mod test {
 
     #[test]
     fn test_solve() {
-        assert_eq!(0, solve(""));
+        assert_eq!(5, solve("0\n3\n0\n1\n-3"));
     }
 
     #[test]
