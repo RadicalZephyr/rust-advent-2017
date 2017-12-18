42a7a49afc01f1a20b731a7d5fa069d2c2b4da3d
Geoff Shannon <geoffpshannon@gmail.com>
2017-12-17 16:26:52 -0800

    Start working on day 5 (generated by a macro!!)

---
+++ src/day_5.rs
@@ -0,0 +1,22 @@
+pub fn solve(_puzzle: &str) -> u32 {
+    0
+}
+
+pub fn solve2(_puzzle: &str) -> u32 {
+    0
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_solve() {
+        assert_eq!(0, solve(""));
+    }
+
+    #[test]
+    fn test_solve2() {
+        assert_eq!(0, solve2(""));
+    }
+}
